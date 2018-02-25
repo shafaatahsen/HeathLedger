42035d56358ed7157aa22cb4afbc3370668d7db5
zanelias <shafaat.ahsen@gmail.com>
2018-02-25 09:39:13 -0600

    Add project files.

---
+++ HeathLedger/wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿// Write your Javascript code.
+$('.message a').click(function () {
+    $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+});
+
+console.log(document.getElementById("password").value);
